940a9f83b4f16d8e85d2bef54afec4e49291efc7
eduardosalvarenga <73663142+eduardosalvarenga@users.noreply.github.com>
2021-01-08 16:51:39 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,10 @@ const api = axios.create({
     baseURL: "http://localhost:8090"
 })
 
+const token = window.sessionStorage.getItem("token");
+
+if (token) {
+  api.defaults.headers.authorization = `Bearer ${token}`;
+}
+
 export default api;
